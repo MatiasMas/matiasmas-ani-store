5f96a98ca0faeb0af1f40ed3faad4dc91e9eb2be
MatiasMas <matias.mas.luberiaga@gmail.com>
2021-11-27 22:01:48 -0300

    Adding NavBar.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <span>AniStore</span>
+            <ul>
+                <a href="#">Highlights</a>
+                <a href="#">Login</a>
+                <a href="#">Register</a>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
